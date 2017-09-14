49a520d554f10439104811fdb02580e0110ca299
lokeshchitturi <lokeshchitturi@gmail.com>
2017-09-14 23:24:14 +0530

    First Java Project uploaded

---
+++ Linkedin/src/main/java/Linkedin/Linkedin/LinkedinLogin.java
@@ -0,0 +1,99 @@
+package Linkedin.Linkedin;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+public class LinkedinLogin {
+
+	WebDriver driver=null;
+	ExtentReports extent=null;
+	ExtentTest logger=null;
+	Properties linkedinProperties=null;
+	
+	@BeforeTest
+
+	public void initializeVariables() {
+		
+		// Extent Report intialization
+		
+		extent=new ExtentReports(System.getProperty("user.dir")+"/test-output/ExtentReport.html", true);
+		extent.addSystemInfo("HostName","LinkedinAutomation")
+			.addSystemInfo("Author","Lokesh")
+			.addSystemInfo("Environment","AutomationTesting");
+		
+		extent.loadConfig(new File(System.getProperty("user.dir")+"/extent-config.xml"));
+	
+		
+		// driver Initialization
+		
+		driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();		
+	
+		// Linkedin Config properties object initialization
+		
+		linkedinProperties=new Properties();
+		
+	}
+	
+	@Test
+	
+	public void loginValidation() throws FileNotFoundException, IOException
+	{
+		
+		linkedinProperties.load(new FileInputStream(System.getProperty("user.dir")+"/LinkedinData.properties"));
+		logger=extent.startTest("LinkedinLogin validation");
+		driver.get("https://www.linkedin.com/");
+		logger.log(LogStatus.INFO, "Linkedin webpage opened");
+		
+		driver.findElement(By.name("session_key")).sendKeys(linkedinProperties.getProperty("linkedinUsername"));
+		
+		logger.log(LogStatus.INFO, "Username entered");
+		
+		driver.findElement(By.name("session_password")).sendKeys(linkedinProperties.getProperty("linkedinPassword"));
+		logger.log(LogStatus.INFO, "Password entered");
+		driver.findElement(By.id("login-submit")).click();
+		logger.log(LogStatus.INFO, "Login button clicked");
+		
+		Assert.assertEquals(driver.findElement(By.linkText(linkedinProperties.getProperty("linkedinProfilename"))).getText(), "Lokesh Chitturi");
+		
+		
+		driver.findElement(By.xpath("//*[@id='nav-settings__dropdown-trigger']/img")).click();
+		logger.log(LogStatus.INFO, "CLiked on dropdown");
+		driver.findElement(By.linkText("Sign out")).click();
+		logger.log(LogStatus.INFO, "CLiked on Signout link");
+		Assert.assertEquals(driver.findElement(By.id("login-submit")).getText(), "Sign in");
+		
+		logger.log(LogStatus.PASS, "Login Logout validation sucessful");
+		
+		
+		
+	}
+	
+	
+	@AfterTest
+	
+	public void close()
+	{
+		driver.close();
+		extent.flush();
+	}
+
+}
